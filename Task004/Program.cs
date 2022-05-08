706c16838d139d611f10fce0448dbd6dde1b3516
ilmir <mr.ilmirka16@mail.ru>
2022-05-09 00:24:22 +0300

    Create task004

---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string str = Console.ReadLine();
+            int a = 1;
+            int b = Convert.ToInt32(str);
+            while(a < b)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine($"Число {a} является четным");
+                }
+                a++;
+            }
+        }
+    }
+}
