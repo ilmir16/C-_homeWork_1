a22545eb2e740bf6d016f5086ba386826fa11976
ilmir <mr.ilmirka16@mail.ru>
2022-05-08 23:37:11 +0300

    Create reposit, .ignore, task001

---
+++ Task001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string sa = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string sb = Console.ReadLine();
+            double a = Convert.ToDouble(sa);
+            double b = Convert.ToDouble(sb);
+            if (a == b * b)
+            {
+                Console.Write($"Число {a} является квадратом числа {b}");
+            }
+            else
+            {
+                Console.Write($"Число {a} не является квадратом числа {b}");
+            }
+        }
+    }
+}
