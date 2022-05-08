a0130d19d1010f1fe05858d37c9224f424ff592f
ilmir <mr.ilmirka16@mail.ru>
2022-05-09 01:07:01 +0300

    Create task005

---
+++ Task005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            string str = Console.ReadLine();
+            int num = Convert.ToInt32(str);
+            Console.WriteLine($"Вторая цифра числа {num} = {(num / 10) % 10}"); 
+        }
+    }
+}
