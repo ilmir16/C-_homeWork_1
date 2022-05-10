a5371b1b1dedf1521dd37c6260bd1a1d8faca5a1
ilmir <mr.ilmirka16@mail.ru>
2022-05-10 13:31:24 +0300

    Create task006

---
+++ Task006/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int value = rand.Next(10, 90);
+            Console.WriteLine($"Рандомное число {value}");
+            int a = value / 10;
+            int b = value % 10;
+            if (a > b)
+            {
+                Console.Write($"Число {a} больше числа {b} ");
+            }
+            else if (b > a)
+            {
+                Console.Write($"Число {b} больше числа {a} ");
+            }
+            else {Console.Write("Числа равны");}
+        }
+    }
+}
