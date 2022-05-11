2c5e479482466bc00b86f78542ea7154f670c106
ilmir <mr.ilmirka16@mail.ru>
2022-05-11 19:11:41 +0300

    Create task 009

---
+++ Task009/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string str = Console.ReadLine();
+            int i = Convert.ToInt32(str);
+            if (i < 100)
+            {
+                Console.WriteLine("3 цифры нету");
+            }
+            else 
+            {
+                while(i > 1000){
+                    i = i / 10;
+                }
+                Console.WriteLine($"Третья цифра равна {i % 10}");
+            }
+
+        }
+    }
+}
