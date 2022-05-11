9e18d5928b281172dcba824ce4f1cfe57ac7ad92
ilmir <mr.ilmirka16@mail.ru>
2022-05-11 17:11:30 +0300

    Create task008

---
+++ Task008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число!");
+            string str1 = Console.ReadLine();
+            int i1 = Convert.ToInt32(str1);
+            Console.Write("Введите второе число!");
+            string str2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(str2);
+            if(i1 % i2 == 0)
+            {
+                Console.WriteLine($"Число {i1} кратно {i2}");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток при делении = {i1 % i2}");
+            }
+        }
+    }
+}
